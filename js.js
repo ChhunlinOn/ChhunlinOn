fd39622f311f3dab935d7c72e91b03c4763a08fa
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2024-03-07 15:38:57 +0700

    sloy

---
+++ js.js
@@ -0,0 +1,10 @@
+function click(){
+    const age = document.getElementById('myinput');
+}
+
+
+//git checkout -b name// create new branch
+//git branch -M main// change name branch
+//git branch -d name// delete branch
+//git checkout name// switch branch
+//git branch -a// see all branch
